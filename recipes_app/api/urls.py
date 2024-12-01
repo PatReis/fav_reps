5ca67a217c3ab8ea5fb679a2ff5dc2218d54fa2b
PatReis <pat.reiser@web.de>
2024-12-01 14:09:56 +0100

    Added downloads.html

--- recipes_app/api/urls.py
+++ recipes_app/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',  views.getRoutes),
-    path('recipes/', views.getRecipes),
-    path('topics/', views.getTopics),
+    path('',  views.getRoutes, name="api_overview"),
+    path('recipes/', views.getRecipes, name="api_recipes"),
+    path('topics/', views.getTopics, name="api_topics"),
     path('recipe/<str:pk>/', views.getRecipe),
 ]
